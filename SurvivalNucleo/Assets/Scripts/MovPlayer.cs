d57b0c5063e7abd72dd8ac99cd39514cb084a902
Zerhe <dante_ro@hotmail.com.ar>
2017-08-22 15:06:43 -0300

    Probando mivimiento y rotaxcion
    Hay que mejorar el movimiento y la rotacion

---
+++ SurvivalNucleo/Assets/Scripts/MovPlayer.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovPlayer : MonoBehaviour {
+    private Rigidbody rgb;
+    private bool forwad;
+    private bool back;
+    private bool right;
+    private bool left;
+    private bool run;
+    private float vel;
+
+    private void Awake()
+    {
+        rgb = GetComponent<Rigidbody>();
+        vel = 100;
+    }
+    void Start ()
+    {
+        forwad = false;
+        back = false;
+        right = false;
+        left = false;
+        run = false;
+	}
+    void FixedUpdate()
+    {
+        if (forwad)
+        {
+            rgb.AddRelativeForce(Vector3.forward * Time.deltaTime * vel, ForceMode.Impulse);
+        }
+
+        if (back)
+        {
+            rgb.AddRelativeForce(Vector3.back * Time.deltaTime * vel, ForceMode.Impulse);
+        }
+
+        if (right)
+        {
+            rgb.AddRelativeForce(Vector3.right * Time.deltaTime * vel, ForceMode.Impulse);
+        }
+
+        if (left)
+        {
+            rgb.AddRelativeForce(Vector3.left * Time.deltaTime * vel, ForceMode.Impulse);
+        }
+
+        if (run)
+        {
+            rgb.AddRelativeForce(Vector3.forward * Time.deltaTime * (vel*2) , ForceMode.Impulse);
+        }
+
+    }
+    void Update ()
+    {
+        if (Input.GetButton("Forwad"))
+        {
+            forwad = true;
+        }
+        else
+            forwad = false;
+        if (Input.GetButton("Back"))
+        {
+            back = true;
+        }
+        else
+            back = false;
+        if (Input.GetButton("Right"))
+        {
+            right = true;
+        }
+        else
+            right = false;
+        if (Input.GetButton("Left"))
+        {
+            left = true;
+        }
+        else
+            left = false;
+        if (Input.GetButton("Run"))
+        {
+            run = true;
+        }
+        else
+            run = false;
+    }
+}
---
+++ SurvivalNucleo/Assets/Scripts/RotPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotPlayer : MonoBehaviour {
+    private float velRot;
+    private Rigidbody rgb;
+
+	void Start () {
+        velRot = 80;
+	}
+    private void Awake()
+    {
+        rgb = GetComponent<Rigidbody>();
+    }
+    void Update () {
+		if (Input.GetAxis("Mouse X") > 0)
+        {
+            rgb.AddRelativeTorque(Vector3.up * Time.deltaTime * velRot);
+        }
+        else if(Input.GetAxis("Mouse X") < 0)
+        {
+            rgb.AddRelativeTorque(Vector3.down * Time.deltaTime * velRot);
+        }
+        if (Input.GetAxis("Mouse Y") > 0)
+        {
+            rgb.AddRelativeTorque(Vector3.left * Time.deltaTime * velRot);
+        }
+        else if (Input.GetAxis("Mouse Y") < 0)
+        {
+            rgb.AddRelativeTorque(Vector3.right * Time.deltaTime * velRot);
+        }
+    }
+}
